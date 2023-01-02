72299cc3cefc784308eed78477f3a80a8a155e4a
Ashutosh Sharma <77958573+ashutosh-buku@users.noreply.github.com>
2023-01-02 13:22:52 +0530

    added usercontroller

--- src/main/java/com/ashu/octopus/controller/DishController.java
+++ src/main/java/com/ashu/octopus/controller/DishController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class DishesOfTheDayController {
+public class DishController {
 
     @Autowired
     private DishService dishService;
---
+++ src/main/java/com/ashu/octopus/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.ashu.octopus.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+}
